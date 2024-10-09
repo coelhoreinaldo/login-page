c842e202c3381c7d63851ace311334285b59075a
coelhoreinaldo <scoelhor7@gmail.com>
2024-10-09 10:55:02 -0300

    feat: add sequelize config

---
+++ backend/src/database/config/database.ts
@@ -0,0 +1,16 @@
+import { Options } from 'sequelize';
+
+const config: Options = {
+  username: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'password',
+  database: process.env.MYSQL_DATABASE || 'api_db',
+  host: process.env.MYSQL_HOST || 'localhost',
+  port: Number(process.env.MYSQL_PORT) || 3306,
+  dialect: 'mysql',
+  dialectOptions: {
+    timezone: 'Z',
+  },
+  logging: false,
+}
+
+export = config;
