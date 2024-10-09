f0a88b1a22482bccbd53fade52ec69692d9ca9c9
coelhoreinaldo <scoelhor7@gmail.com>
2024-10-09 16:56:51 -0300

    refactor: seeds and add scripts

--- backend/src/database/seeders/01-users.ts
+++ backend/src/database/seeders/01-users.ts
@@ -4,13 +4,13 @@ export default {
   up: async (queryInterface: QueryInterface) => {
     await queryInterface.bulkInsert('users', [
       {
-        email: 'user1@email.com',
-        password: 'password1',
+        email: 'user1@email.teste',
+        password: '$2b$10$ZkncXPwWP1B50KS3ZS4C8u427UBCvB3PesdOBkYwEsvWVJCmp4ZPq', // user11
         name: 'User 1',
       },
       {
-        email: 'user2@email.com',
-        password: 'password2',
+        email: 'user2@email.teste',
+        password: '$2b$10$zvLtghznOSvQ3FRtMiWNaOVGlWOc0q/PRYCw83cOwF51Xapt54BBa', //user22
         name: 'User 2',
       },
     ], {});
