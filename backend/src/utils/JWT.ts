89c041407afd8341b604de69e1be156443095af4
coelhoreinaldo <scoelhor7@gmail.com>
2024-10-10 09:16:40 -0300

    chore: add backend to docker-compose

--- backend/src/utils/JWT.ts
+++ backend/src/utils/JWT.ts
@@ -1,6 +1,6 @@
 import { JwtPayload, Secret, sign, SignOptions, verify } from 'jsonwebtoken';
 export default class JWT {
-  private static secret: Secret = process.env.JWT_SECRET || '';
+  private static secret: Secret = process.env.JWT_SECRET || 'secret';
 
   private static jwtConfig: SignOptions = {
     expiresIn: '10d',
