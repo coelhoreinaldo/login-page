97fcef7054a4483a485527e2dd8ffc3a3e6c7001
coelhoreinaldo <scoelhor7@gmail.com>
2024-10-10 11:59:01 -0300

    style: format

--- frontend/src/components/Profile.vue
+++ frontend/src/components/Profile.vue
@@ -1,45 +1,45 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
-import { useRouter } from 'vue-router';
-import axios from 'axios';
-import { useStore } from 'vuex';
+import { ref, onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import axios from 'axios'
+import { useStore } from 'vuex'
 import type { User } from '../interfaces'
 
-const store = useStore();
-const router = useRouter();
-const profileData = ref<User | null>(null);
-const errorMessage = ref('');
+const store = useStore()
+const router = useRouter()
+const profileData = ref<User | null>(null)
+const errorMessage = ref('')
 
 const fetchProfile = async () => {
   try {
     const response = await axios.get('http://localhost:3001/api/profile', {
       headers: {
         Authorization: `Bearer ${store.getters.getToken}`
-      },
-    });
-    profileData.value = response.data;
+      }
+    })
+    profileData.value = response.data
   } catch (error) {
-    console.error('Erro ao buscar perfil:', error);
+    console.error('Erro ao buscar perfil:', error)
     if (axios.isAxiosError(error) && error.response && error.response.status === 401) {
-      store.commit('setToken', '');
-      localStorage.removeItem('token');
-      router.push('/login');
+      store.commit('setToken', '')
+      localStorage.removeItem('token')
+      router.push('/login')
     } else {
-      errorMessage.value = 'Erro ao carregar perfil. Por favor, tente novamente.';
+      errorMessage.value = 'Erro ao carregar perfil. Por favor, tente novamente.'
     }
   }
-};
+}
 
 onMounted(() => {
   if (store.getters.isAuthenticated) {
-    fetchProfile();
+    fetchProfile()
   } else {
-    errorMessage.value = 'Email não encontrado no token. Por favor, faça login novamente.';
-    store.commit('setToken', '');
-    localStorage.removeItem('token');
-    router.push('/login');
+    errorMessage.value = 'Email não encontrado no token. Por favor, faça login novamente.'
+    store.commit('setToken', '')
+    localStorage.removeItem('token')
+    router.push('/login')
   }
-});
+})
 </script>
 
 <template>
@@ -49,7 +49,7 @@ onMounted(() => {
       <p><strong>Email:</strong> {{ profileData.email }}</p>
       <p><strong>Nome:</strong> {{ profileData.name }}</p>
     </div>
-    <div v-if="errorMessage" style="color: red;">{{ errorMessage }}</div>
+    <div v-if="errorMessage" style="color: red">{{ errorMessage }}</div>
   </main>
 </template>
 
